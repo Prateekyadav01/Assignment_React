8b7b77316976ef2253604524209ee459ca8806d8
PrateekTechmatys <prateeky562@gmail.com>
2024-04-27 16:18:32 +0530

    done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,9 +11,9 @@ const Card = ({ data, nodata, }) => {
       console.log(data.name)
   }
   return (
-    <div className="bg-black rounded-lg shadow-md p-4 mt-4 mb-4 flex items-center justify-center ml-4 w-[90%]">
+    <div  onClick={handleUserName} className="bg-black rounded-lg shadow-md p-4 mt-4 mb-4 flex items-center justify-center ml-4 w-[90%]">
 
-      <h1 className="text-xl font-bold text-white" onClick={handleUserName}>{data.name}</h1>
+      <h1 className="text-xl font-bold text-white">{data.name}</h1>
     </div>
   );
 };
